8dd0ddaa309f0d6e1deec3d63adb13ebfdd339fb
thuongnguyen <thuongnguyen016685@gmail.com>
2021-02-26 20:16:04 +0700

    Fix dog chalk

--- Dog.js
+++ Dog.js
@@ -1,4 +1,4 @@
- var chalk = require('chalk');
+
  
  function Dog(name){
 	this.name = name;
@@ -8,6 +8,6 @@ Dog.prototype.eat = function(Cat) {
 	this.stomach.push(Cat);
 };
 Dog.prototype.sayHi = function() {
-	console.log('Hi! i am a dog.My name is'+ chalk.blue(this.name))
+	console.log('Hi! i am a dog.My name is'+this.name)
 };
 module.exports = Dog;
